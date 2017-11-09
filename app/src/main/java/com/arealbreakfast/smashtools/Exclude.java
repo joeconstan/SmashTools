2bdd38c8d32dec60c5d3400ec2f27529b23c62b1
Joe <jconstan@bu.edu>
2017-11-09 14:09:43 -0600

    character exludes

--- app/src/main/java/com/arealbreakfast/smashtools/Exclude.java
+++ app/src/main/java/com/arealbreakfast/smashtools/Exclude.java
@@ -1,5 +1,6 @@
 package com.arealbreakfast.smashtools;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -7,9 +8,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.GridView;
-import android.widget.ImageButton;
-import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 
 public class Exclude extends AppCompatActivity {
@@ -23,7 +25,7 @@ public class Exclude extends AppCompatActivity {
         grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                
+                //remove from view and list passed to other thing
             }
         });
 
@@ -33,10 +35,12 @@ public class Exclude extends AppCompatActivity {
 
 class ButtonAdapter extends BaseAdapter {
 
+    private ArrayList<Integer> mThumbIds = new ArrayList<>(58);
+
 
     @Override
     public int getCount() {
-        return 0;
+        return mThumbIds.size();
     }
 
     @Override
@@ -52,12 +56,70 @@ class ButtonAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        ImageButton i = new ImageButton(convertView.getContext());
+        Button btn = new Button(convertView.getContext());
 
-        i.setImageResource(mFams.get(position).imageId);
-        i.setScaleType(ImageView.ScaleType.FIT_CENTER);
-        final int w = (int) (36 * getResources().getDisplayMetrics().density + 0.5f);
-        i.setLayoutParams(new GridView.LayoutParams(w * 2, w * 2));
-        return i;
+        mThumbIds.add(R.drawable.bayo);
+        mThumbIds.add(R.drawable.bowser);
+        mThumbIds.add(R.drawable.bowserjr);
+        mThumbIds.add(R.drawable.captainfalcon);
+        mThumbIds.add(R.drawable.charizard);
+        mThumbIds.add(R.drawable.cloud);
+        mThumbIds.add(R.drawable.corrin);
+        mThumbIds.add(R.drawable.darkpit);
+        mThumbIds.add(R.drawable.drmario);
+        mThumbIds.add(R.drawable.duckhunt);
+        mThumbIds.add(R.drawable.falco);
+        mThumbIds.add(R.drawable.fox);
+        mThumbIds.add(R.drawable.ganon);
+        mThumbIds.add(R.drawable.greninja);
+        mThumbIds.add(R.drawable.ike);
+        mThumbIds.add(R.drawable.kingddd);
+        mThumbIds.add(R.drawable.kirby);
+        mThumbIds.add(R.drawable.link);
+        mThumbIds.add(R.drawable.littlemac);
+        mThumbIds.add(R.drawable.lucario);
+        mThumbIds.add(R.drawable.lucas);
+        mThumbIds.add(R.drawable.lucina);
+        mThumbIds.add(R.drawable.luigi);
+        mThumbIds.add(R.drawable.mario);
+        mThumbIds.add(R.drawable.marth);
+        mThumbIds.add(R.drawable.megaman);
+        mThumbIds.add(R.drawable.metaknight);
+        mThumbIds.add(R.drawable.mewtwo);
+        mThumbIds.add(R.drawable.miibrawler);
+        mThumbIds.add(R.drawable.miigunner);
+        mThumbIds.add(R.drawable.miiswordfighter);
+        mThumbIds.add(R.drawable.mrgnw);
+        mThumbIds.add(R.drawable.ness);
+        mThumbIds.add(R.drawable.olimar);
+        mThumbIds.add(R.drawable.pacman);
+        mThumbIds.add(R.drawable.palutena);
+        mThumbIds.add(R.drawable.peach);
+        mThumbIds.add(R.drawable.pikachupic);
+        mThumbIds.add(R.drawable.pit);
+        mThumbIds.add(R.drawable.puff);
+        mThumbIds.add(R.drawable.rob);
+        mThumbIds.add(R.drawable.robin);
+        mThumbIds.add(R.drawable.rosalina);
+        mThumbIds.add(R.drawable.roy);
+        mThumbIds.add(R.drawable.ryupic);
+        mThumbIds.add(R.drawable.samus);
+        mThumbIds.add(R.drawable.sheik);
+        mThumbIds.add(R.drawable.shulk);
+        mThumbIds.add(R.drawable.sonic);
+        mThumbIds.add(R.drawable.toonlink);
+        mThumbIds.add(R.drawable.villager);
+        mThumbIds.add(R.drawable.wario);
+        mThumbIds.add(R.drawable.wft);
+        mThumbIds.add(R.drawable.yoshi);
+        mThumbIds.add(R.drawable.zelda);
+        mThumbIds.add(R.drawable.zss);
+        btn.setBackgroundResource(mThumbIds.get(position));
+        btn.setTextColor(Color.WHITE);
+        btn.setId(position);
+        btn.setFocusableInTouchMode(false);
+        btn.setFocusable(false);
+        btn.setClickable(false);
+        return btn;
     }
 }
