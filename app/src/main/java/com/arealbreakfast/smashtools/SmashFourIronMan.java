cdac37d4cd34f6ba3858a83f9e8da39eeda80bc4
Joe <jconstan@bu.edu>
2017-12-07 10:36:57 -0600

    better user input and UI

--- app/src/main/java/com/arealbreakfast/smashtools/SmashFourIronMan.java
+++ app/src/main/java/com/arealbreakfast/smashtools/SmashFourIronMan.java
@@ -6,10 +6,11 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Random;
 
 public class SmashFourIronMan extends AppCompatActivity {
@@ -20,16 +21,58 @@ public class SmashFourIronMan extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smashfourironman);
 
+        final TextView numplayer = (TextView) findViewById(R.id.s4imnumber_play);
+        final TextView numchar = (TextView) findViewById(R.id.s4imnumber_char);
+        ImageButton incplay = (ImageButton) findViewById(R.id.increase_play);
+        ImageButton decplay = (ImageButton) findViewById(R.id.decrease_play);
+        ImageButton incchar = (ImageButton) findViewById(R.id.increase_char);
+        ImageButton decchar = (ImageButton) findViewById(R.id.decrease_char);
+
+        incplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int n = Integer.parseInt(numplayer.getText().toString());
+                n++;
+                numplayer.setText(String.valueOf(n));
+            }
+        });
+
+        decplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int n = Integer.parseInt(numplayer.getText().toString());
+                n--;
+                numplayer.setText(String.valueOf(n));
+            }
+        });
+
+        incchar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int n = Integer.parseInt(numchar.getText().toString());
+                n++;
+                numchar.setText(String.valueOf(n));
+            }
+        });
+
+        decchar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int n = Integer.parseInt(numchar.getText().toString());
+                n--;
+                numchar.setText(String.valueOf(n));
+            }
+        });
 
     }
 
 
     public void Start(View view) {
-        EditText players = (EditText) findViewById(R.id.s4imnumber_play);
+        TextView numplayer = (TextView) findViewById(R.id.s4imnumber_play);
         //pick characters randomly and display in Results activity
-        EditText num = (EditText) findViewById(R.id.s4imnumber_char);
-        int x = Integer.parseInt(num.getText().toString());
-        int y = Integer.parseInt(players.getText().toString());
+        TextView numchar = (TextView) findViewById(R.id.s4imnumber_char);
+        int x = Integer.parseInt(numchar.getText().toString());
+        int y = Integer.parseInt(numplayer.getText().toString());
         ArrayList<Integer> chosen_chars = new ArrayList<>(1);
         Random randomGenerator = new Random();
         int ranNum;
@@ -45,7 +88,7 @@ public class SmashFourIronMan extends AppCompatActivity {
 
         Intent intent = new Intent(view.getContext(), Results.class);
         intent.putExtra("chars", chosen_chars);
-        intent.putExtra("players", Integer.parseInt(players.getText().toString()));
+        intent.putExtra("players", Integer.parseInt(numplayer.getText().toString()));
         intent.putExtra("type", 0);
 
         startActivity(intent);
